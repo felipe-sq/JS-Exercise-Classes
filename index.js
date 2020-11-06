556d205adf1b416b033f561c3403fb6bf1320950
Felipe Quintero <ffquint@gmail.com>
2020-11-05 21:39:33 -0500

    completed code for task 6. 1 test for .grade method on ProjectManager is not passing, even though the test passes for the Instructor (where the method is written).

--- index.js
+++ index.js
@@ -206,8 +206,21 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  };
+  standUp(channel){
+    this.channel = channel;
+    return `${this.name} announces to ${this.channel}, @channel standy times!.`;
+  };
+  debugsCode(student, subject){
+    this.student = student;
+    this.subject = subject;
+    return `${this.name} debugs ${this.student.name}'s code on ${this.subject}.`;
+  };
 }
 
 /*
